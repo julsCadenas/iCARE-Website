3c108cd9144430b769555acda752e335195fa4db
Julian Cadenas <jscadenas12@gmail.com>
2024-03-31 17:42:53 +0800

    first page

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/index-page/index-style.css">
+    <link rel="icon" type="image/png" href="/images/feutech.PNG">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+</head>
+    <title>iCARE</title>
+</head>
+<body>
+    <div class="template">
+        <img src="/images/tams.PNG" alt="tams" id="tams">
+        <img src="/images/dahon.JPG" alt="dahon" id="dahon">
+        <img src="/images/icare.PNG" alt="icare" id="icare">
+        <img src="/images/feutech.PNG" alt="feutech" id="feutech">
+    </div> 
+
+    <div class="buttons">
+        <button class="StudentButton" onclick="student()">Student</button>
+        <button class="StaffButton" onclick="staff()">Staff</button>
+    </div>
+
+    <script src="/index-page/index-script.js"></script>
+</body>
+</html>
